35d644155c0cbc26b14b3cc31905bf5a22605dc4
Mohamed-Chaabene <100580744+Mohamed-Chaabene@users.noreply.github.com>
2026-04-05 09:00:39 +0200

    search and notification works
    search and notification works

--- src/main/java/t/esprit/arctic/jobmatch/repository/UtilisateurRepository.java
+++ src/main/java/t/esprit/arctic/jobmatch/repository/UtilisateurRepository.java
@@ -1,13 +1,26 @@
 package t.esprit.arctic.jobmatch.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
+import t.esprit.arctic.jobmatch.entity.Role;
 import t.esprit.arctic.jobmatch.entity.Utilisateur;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
-
     Optional<Utilisateur> findByEmail(String email);
+
+    Optional<Utilisateur> findByEmailIgnoreCase(String email);
+
+    List<Utilisateur> findByNomIgnoreCase(String nom);
+
+    List<Utilisateur> findByNomStartingWithIgnoreCase(String nom);
+
+    List<Utilisateur> findByNomContainingIgnoreCase(String nom);
+
+    List<Utilisateur> findByRole(Role role);
+
+    List<Utilisateur> findByRoleIn(List<Role> roles);
 
 }
